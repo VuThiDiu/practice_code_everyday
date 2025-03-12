ea18dc712442567ceb63870d12d1ced26190c13a
diuvt1 <diuvt1@vnpay.vn>
2025-03-12 16:28:45 +0700

    add gitignore and problem : 'getconcatenatio'

---
+++ src/leetcode/LCArray.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class LCArray {
+
+    /*
+    link: https://leetcode.com/problems/concatenation-of-array/
+    Level: Easy
+    * */
+    public int[] getConcatenation(int[] nums) {
+        int length = nums.length;
+        int[] result = new int[length * 2];
+        for(int i = 0; i < length; i++){
+            result[i] = nums[i];
+            result[i+ length] = nums[i];
+        }
+        return result;
+    }
+
+}
