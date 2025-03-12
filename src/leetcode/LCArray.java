d329cd9e1983ff3399851e28a2cdbf6acbb64af8
diuvt1 <diuvt1@vnpay.vn>
2025-03-12 16:42:32 +0700

    solve pro : 'replace Elements'

--- src/leetcode/LCArray.java
+++ src/leetcode/LCArray.java
@@ -9,11 +9,28 @@ public class LCArray {
     public int[] getConcatenation(int[] nums) {
         int length = nums.length;
         int[] result = new int[length * 2];
-        for(int i = 0; i < length; i++){
+        for (int i = 0; i < length; i++) {
             result[i] = nums[i];
-            result[i+ length] = nums[i];
+            result[i + length] = nums[i];
         }
         return result;
     }
+
+    /*
+    link: https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+    Level: Easy 
+    * */
+    public static int[] replaceElements(int[] arr) {
+        int maxInRight = -1;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int temp = arr[i];
+            arr[i] = maxInRight;
+            maxInRight = Math.max(temp, maxInRight);
+        }
+        return arr;
+    }
+
+
+
 
 }
